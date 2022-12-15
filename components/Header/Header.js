0d07628771882ba47409fe00048762318f8d5d6c
Oliver Kusch <oliver.kusch@isgus.de>
2022-12-15 11:55:11 +0100

    Final commit

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -10,8 +10,8 @@ import {
   HeartIcon,
 } from "@heroicons/react/24/outline";
 import { useRecoilState } from "recoil";
-import { modalState } from "../../atoms/modalAtom"
-import MobileMenu from '../Header/MobileMenu'
+import { modalState } from "../../atoms/modalAtom";
+import MobileMenu from "../Header/MobileMenu";
 
 function Header() {
   const { data: session, status } = useSession();
@@ -19,33 +19,44 @@ function Header() {
   const router = useRouter();
 
   return (
-    <header className="border-b-0 border-gray-300 shadow-sm bg-white sticky top-0 z-50">
+    <header className="border-b-0 pt-3 pb-3 border-gray-300 shadow-sm bg-white sticky top-0 z-50">
       <div className="max-w-6xl m-auto">
         <div className="flex justify-between items-center mx-5">
-          <div onClick={() => router.push('/')} className="relative hidden lg:inline-grid w-24 cursor-pointer">
+          <div
+            onClick={() => router.push("/")}
+            className="relative hidden lg:inline-grid w-24 cursor-pointer"
+          >
             <img src="https://links.papareact.com/ocw" alt="Instagram Logo" />
           </div>
-          <div onClick={() => router.push('/')} className="relative inline-grid lg:hidden w-10 h-10 flex-shrink-0 cursor-pointer">
+          <div
+            onClick={() => router.push("/")}
+            className="relative inline-grid lg:hidden w-10 h-10 flex-shrink-0 cursor-pointer"
+          >
             <img src="https://links.papareact.com/jjm" alt="Instagram Logo" />
           </div>
 
-          <div className="max-w-xs">
-            <div className="mt-1 relative bg-whitet p-3">
-              <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
-                <MagnifyingGlassIcon className="w-5 h-5" />
+          {!session && (
+            <div className="max-w-xs">
+              <div className="bg-white">
+                <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+                  <MagnifyingGlassIcon className="w-5 h-5" />
+                </div>
+                <input
+                  className="bg-gray-50  border-gray-300 rounded-md focus:ring-black focus:border-black block w-full pl-10"
+                  type="text"
+                  placeholder="Search"
+                />
               </div>
-              <input
-                className="bg-gray-50  border-gray-300 rounded-md focus:ring-black focus:border-black block w-full pl-10"
-                type="text"
-                placeholder="Search"
-              />
             </div>
-          </div>
+          )}
 
           <div className="flex items-center justify-end space-x-4">
             {/* Mobile Dropdown Menu */}
             <MobileMenu />
-            <HomeIcon onClick={() => router.push('/')} className="header-icons" />
+            <HomeIcon
+              onClick={() => router.push("/")}
+              className="header-icons"
+            />
             {/* <Bars3BottomRightIcon className=" h-5 w-5 cursor-pointer md:hidden inline-flex hover:scale-125 transition-all duration-150 ease-out" />  */}
 
             {session ? (
@@ -57,11 +68,14 @@ function Header() {
                   </div>
                 </div>
 
-                <PlusCircleIcon onClick={() => setShowModal(true)} className="header-icons" />
+                <PlusCircleIcon
+                  onClick={() => setShowModal(true)}
+                  className="header-icons"
+                />
                 <UserGroupIcon className="header-icons" />
                 <HeartIcon className="header-icons" />
                 <img
-                  onClick={signOut({callbackUrl: 'auth/signin'})}
+                  onClick={signOut({ callbackUrl: "auth/signin" })}
                   src={session?.user?.image}
                   alt="profile-picture"
                   className="h-8 rounded-full w-8 md:h-10 md:w-10 cursor-pointer inline-flex"
